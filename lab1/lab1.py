468d338c35c67b630512d2a2bff7f8eb6a0fd49b
KarolinaTribunskaya <78512320+KarolinaTribunskaya@users.noreply.github.com>
2021-02-14 01:10:47 +0300

    Add files via upload

---
+++ lab1/lab1.py
@@ -0,0 +1,73 @@
+import random
+from openpyxl import *
+wb = Workbook()
+a0 = 1
+a1 = 2
+a2 = 3
+a3 = 4
+
+x1 = []
+x2 = []
+x3 = []
+
+xn1 = []
+xn2 = []
+xn3 = []
+
+for i in range(8):
+    x1.append(random.randint(0, 20))
+    x2.append(random.randint(0, 20))
+    x3.append(random.randint(0, 20))
+
+print(f"x1 = {x1}")
+print(f"x2 = {x2}")
+print(f"x3 = {x3}")
+
+x1_min = min(x1)
+x2_min = min(x2)
+x3_min = min(x3)
+
+x1_max = max(x1)
+x2_max = max(x2)
+x3_max = max(x3)
+
+x01 = (x1_max + x1_min)/2
+x02 = (x2_max + x2_min)/2
+x03 = (x3_max + x3_min)/2
+
+dx_1 = x1_max - x01
+dx_2 = x2_max - x02
+dx_3 = x3_max - x03
+
+for i in range(len(x1)):
+    xn1.append((x1[i] - x01)/dx_1)
+    xn2.append((x2[i] - x02)/dx_2)
+    xn3.append((x3[i] - x03)/dx_3)
+
+y = []
+for i in range(len(x1)):
+    y.append(a0 + a1*x1[i] + a2*x2[i] + a3*x3[i])
+
+print(f"y = {y}")
+
+#for i in range(len(x1)):
+#    xn1[i] = round(xn1[i])
+#    xn2[i] = round(xn2[i])
+#    xn3[i] = round(xn3[i])
+
+y_etalon = a0 + a1*x01 + a2*x02 + a3*x03
+
+
+print(f"xn1 = {xn1}")
+print(f"xn2 = {xn2}")
+print(f"xn3 = {xn3}")
+
+print(f"y_etalon = {y_etalon}")
+
+expression = []
+
+for i in range(len(y)):
+    expression.append((y[i] - y_etalon)**2)
+
+print(f"expression = {expression}")
+print(f"optimal value = {max(expression)}")
